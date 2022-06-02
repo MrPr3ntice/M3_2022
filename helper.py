cc0516e990a382259e7c76e6ecc1810157d397d5
Philipp Kortmann <20466981+MrPr3ntice@users.noreply.github.com>
2022-06-02 19:39:36 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def imshow(inp, title=None)
+def imshow(inp, title=None):
     """Imshow for pytorch-tensor."""
     inp = inp.numpy().transpose((1, 2, 0))
     mean = np.array([0.485, 0.456, 0.406])
