2aeb6b26f30da1c07095456cad86a817efcb5cb4
Philipp Kortmann <20466981+MrPr3ntice@users.noreply.github.com>
2022-06-02 19:35:45 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,18 +1,5 @@
-from __future__ import print_function, division
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import torch.backends.cudnn as cudnn
 import numpy as np
-import torchvision
-from torchvision import datasets, models, transforms
-from google.colab import drive
 import matplotlib.pyplot as plt
-import time
-import os
-import copy
 
 def imshow(inp, title=None):
     """Imshow for pytorch-tensor."""
