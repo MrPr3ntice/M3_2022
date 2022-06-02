263bc193c8d97a76b78c7ed52d3297614b639658
Philipp Kortmann <20466981+MrPr3ntice@users.noreply.github.com>
2022-06-02 19:03:44 +0200

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,27 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from google.colab import drive
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
